dd487e34b77a9a46571a28a9150bc273df6ffb4f
Yuriy Gritsenko <yuriyg@intercompcompany.com>
2012-07-20 09:04:36 -0500

    First Remote Commit

---
+++ Hello-World/Hello-World/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  Hello-World
+//
+//  Created by Yuriy Gritsenko on 7/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello-World/Hello-World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello-World
+//
+//  Created by Yuriy Gritsenko on 7/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
